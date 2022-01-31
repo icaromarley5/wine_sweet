383d0467f0753e0aae4f40fc8dc70a4c2832478d
Ícaro <icaromarley5@gmail.com>
2022-01-30 23:02:38 -0300

    fix: Remover código legado

--- wine_sweet/spiders/wine_spider.py
+++ wine_sweet/spiders/wine_spider.py
@@ -2,44 +2,6 @@ import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import Rule
 
-"""
-
-class WineSpider(scrapy.Spider):
-    name = "Wine"
-
-    url_base = "https://www.wine.com.br"
-    url_base_navigation = (
-        url_base
-        + "/browse.ep?cID=100851&exibirEsgotados=false&pn={page}&listagem=horizontal&sorter=featuredProducts-desc&filters=cVINHOS"
-    )
-    start_urls = [url_base_navigation.format(page=1)]
-    last_page = None
-
-    def parse(self, response):
-        if not self.last_page:
-            self.last_page = int(
-                response.xpath(
-                    '//div[contains(@class, "Pagination")]/ul/li[last()]/a/text()'
-                ).get()
-            )
-            response.css("div.Pagination").get()
-
-        links = [
-            self.url_base + link
-            for link in response.xpath(
-                "//a[contains(@class, 'js-productClick')]/@href"
-            ).getall()
-        ]
-        yield {"links": links}
-
-        page_links = [
-            self.url_base_navigation.format(page=page)
-            for page in range(2, self.last_page + 1)
-        ]
-        for link in page_links:
-            yield scrapy.Request(link, callback=self.parse)
-        """
-
 
 class WineSpider(scrapy.spiders.CrawlSpider):
     name = "Wine"
